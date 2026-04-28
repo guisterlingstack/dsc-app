fb0221873083cea253fdcc2406237d3ff3bc7c07
guisterlingstack <guisterlingfin@gmail.com>
2026-04-28 02:38:10 -0300

    Fix SPA routing native mode

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
-import { writeFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'generate-redirects',
-      closeBundle() {
-        writeFileSync('dist/_redirects', '/* /index.html 200\n');
-      },
-    },
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
